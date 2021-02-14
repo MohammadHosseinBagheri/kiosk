73dc547db04126374ad5bd8905242d6f119062fd
Alireza <real7alireza@gmail.com>
2021-02-15 02:22:45 +0330

    ltr drawer toggle

--- src/components/navigation/toolbar.js
+++ src/components/navigation/toolbar.js
@@ -6,13 +6,13 @@ import DrawerToggle from "./sideDrawer/drawerToggle/drawerToggle";
 const toolbar = (props) => {
   return (
     <header className="toolbar">
-      <DrawerToggle clicked={props.drawerToggleClicked} />
       <div className="desktopOnly">
         <NavLeft />
       </div>
       <nav className="desktopOnly">
         <NavigationItems />
       </nav>
+      <DrawerToggle clicked={props.drawerToggleClicked} />
     </header>
   );
 };
