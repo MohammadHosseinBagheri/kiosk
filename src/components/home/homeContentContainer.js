b45de741c91f6c6ed3671e486bdb32a0d0b91fe1
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-02-14 20:12:37 +0330

    useFetch

--- src/components/home/homeContentContainer.js
+++ src/components/home/homeContentContainer.js
@@ -1,6 +1,11 @@
+import useFetch from "../../hooks";
 import Slider from "./../Slider/Slider";
 
 const HomeContentContainer = () => {
+  const { error, success, data, isFetching } = useFetch(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log(error, success, data, isFetching);
   return (
     <div>
       <Slider />
