3ef9d5d3e54e0fb4f36057b29a2e6c1485276699
real7alireza <real7alireza@gmail.com>
2021-02-14 15:09:05 +0330

    toolbar

---
+++ src/components/navigation/toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavigationItems from './navigationItems/navigationItems'
+
+const toolbar = () => {
+    return (
+        <header>
+            <div>
+                {/* ورود */}
+            </div>
+            <nav>
+                <NavigationItems />                
+            </nav>
+        </header>
+    )
+}
+
+export default toolbar
