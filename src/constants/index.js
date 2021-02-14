e47c9529fabe7d90377314ba3b0cabd3b2f76cc2
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-02-14 20:13:02 +0330

    fetch initial state

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,9 @@
-export const bestPrograms = 'بهترین برنامه ها'
+const bestPrograms = "بهترین برنامه ها";
+const FetchState = {
+  error: false,
+  success: false,
+  isFetching: false,
+  data: [],
+};
+
+export { bestPrograms, FetchState };
