c9d02b7a1c8ad78cc459e02d0d947a09cee0ab2a
real7alireza <real7alireza@gmail.com>
2021-02-14 22:13:50 +0330

    drawer toggle

--- src/components/navigation/toolbar.js
+++ src/components/navigation/toolbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavigationItems from './navigationItems/navigationItems'
 import NavLeft from './navLeft/navLeft'
+import DrawerToggle from './drawerToggle/drawerToggle'
 
 const toolbar = () => {
     return (
         <header className='toolbar'>
+            <DrawerToggle />
             <NavLeft />
             <nav>
                 <NavigationItems />
