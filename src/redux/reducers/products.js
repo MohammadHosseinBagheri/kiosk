823f7ec20abab41425f841fae8d4a3580c4c6fe8
real7alireza <real7alireza@gmail.com>
2021-02-16 19:26:06 +0330

    reducer receive best and newest products

--- src/redux/reducers/products.js
+++ src/redux/reducers/products.js
@@ -1,9 +1,32 @@
 import * as types from "./../../constants/actionTypes";
 
-const products = (state = {}, action) => {
+const products = (state = [], action) => {
+
+  const { type, products } = action
+
   switch (action.type) {
+
     case types.RECEIVE_PRODUCTS:
-      return action.products;
+      return products;
+
+    case types.RECEIVE_BEST_PRODUCTS:
+      const bestProducts = products.reduce((obj, product) => {
+        if (product.categori === categori.best) {
+          obj.push(product)
+        }
+        return obj
+      }, [])
+      return bestProducts
+
+    case types.RECEIVE_BEST_PRODUCTS:
+      const newestProducts = products.reduce((obj, product) => {
+        if (product.categori === categori.newest) {
+          obj.push(product)
+        }
+        return obj
+      }, [])
+      return newestProducts
+
 
     default:
       return state;
@@ -11,3 +34,7 @@ const products = (state = {}, action) => {
 };
 
 export default products;
+
+
+
+
