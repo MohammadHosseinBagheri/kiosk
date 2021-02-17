2184a2711cf0b6f8ef1614651ac55007e43da94c
real7alireza <real7alireza@gmail.com>
2021-02-17 22:09:05 +0330

    breackpoint for swiper

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -5,6 +5,7 @@ import SwiperCore, { Navigation } from 'swiper'
 import 'swiper/swiper-bundle.css'
 
 
+
 const Slider = (props) => {
 
   const { products } = props
@@ -13,9 +14,30 @@ const Slider = (props) => {
 
   const swiper = products.length > 0
     ? <Swiper
+      className='slider'
       navigation
-      spaceBetween={20}
-      slidesPerView={5}
+      spaceBetween={10}
+      slidesPerView={1}
+      breakpoints={{
+        270: {
+          slidesPerView: 1,
+        },
+        368: {
+          slidesPerView: 2,
+        },
+        576: {
+          slidesPerView: 4,
+        },
+        768: {
+          slidesPerView: 5,
+        },
+        992: {
+          slidesPerView: 6,
+        },
+        1200: {
+          slidesPerView: 7,
+        },
+      }}
     >
       {products.map(product => (
         <SwiperSlide key={product._id}>
