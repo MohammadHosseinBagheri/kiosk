123b0d7b3e162a5dc19bdc3b761e6524b274eead
real7alireza <real7alireza@gmail.com>
2021-02-17 16:46:27 +0330

    remove react-elastic-carousel

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,33 +1,14 @@
 import React from "react";
 import Item from "./item/item";
-import Carousel from "react-elastic-carousel";
-
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 300, itemsToShow: 2 },
-  { width: 550, itemsToShow: 3 },
-  { width: 768, itemsToShow: 4 },
-  { width: 1000, itemsToShow: 5 },
-  { width: 1200, itemsToShow: 6 },
-];
 
 const Slider = (props) => {
   const { products } = props
-  let showArrow = window.innerWidth < 768 ? false : true;
+
   return (
     <div className="slider">
       <h1 className="sliderHeader"></h1>
       <div className="sliderContent">
-        <Carousel
-          breakPoints={breakPoints}
-          isRTL={true}
-          pagination={false}
-          showArrows={showArrow}
-        >
-          {products.map(product => (
-            <Item key={product._id} {...product} />
-          ))}
-        </Carousel>
+
       </div>
     </div>
   );
