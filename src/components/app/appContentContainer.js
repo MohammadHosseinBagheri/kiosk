a09b296055337d15a7fefb663f6da7c5b8676e7a
real7alireza <real7alireza@gmail.com>
2021-02-22 18:24:47 +0330

    add local img in app

--- src/components/app/appContentContainer.js
+++ src/components/app/appContentContainer.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { Container, Row, Col } from 'react-bootstrap'
+import img from './../../assets/icon/app.png'
+
 
 const AppContentContainer = props => {
 
@@ -25,10 +27,12 @@ const AppContentContainer = props => {
         <Container dir='rtl'>
             <Row>
                 <Col sm={12} md={6}>
-                    <Row>
-                        <Col xs={12} sm={6}>icon</Col>
-                        <Col xs={12} sm={6}>
-                            <Row className="text-sm-center text-md-right">
+                    <Row className='d-flex justify-content-center'>
+                        <Col xs={12} sm={6} className='text-center'>
+                            <img style={{width: '100%' ,maxWidth: '200px'}} src={img} />
+                        </Col>
+                        <Col xs={12} sm={6} className='d-flex justify-content-center align-items-center'>
+                            <Row className="text-center text-md-right">
                                 <Col xs={12} sm={12}>name</Col>
                                 <Col xs={12} sm={12}>company</Col>
                                 <Col xs={12} sm={12}>version</Col>
@@ -37,7 +41,7 @@ const AppContentContainer = props => {
                         </Col>
                     </Row>
                 </Col>
-                <Col sm={12} md={6} className='d-flex align-items-center justify-content-end'>
+                <Col sm={12} md={6} className='d-flex align-items-center justify-content-center justify-content-md-end'>
                     <Row>
                         <Col>rate</Col>
                         <Col>categori</Col>
