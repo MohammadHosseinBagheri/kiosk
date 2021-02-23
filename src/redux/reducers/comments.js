dea5b25f87890a3387b7181764cbb44824472b97
real7alireza <real7alireza@gmail.com>
2021-02-23 21:12:25 +0330

    reducer add comment

--- src/redux/reducers/comments.js
+++ src/redux/reducers/comments.js
@@ -4,6 +4,11 @@ const comments = (state = [], action) => {
     switch (action.type) {
         case types.RECIEVE_COMMENTS:
             return action.comments
+        case types.ADD_COMMENT:
+            return [
+                action.comment,
+                ...state
+            ]
         default:
             return state
     }
