21ec6f807090a547007b48343ebde64b77975477
real7alireza <real7alireza@gmail.com>
2021-02-25 16:20:22 +0330

    reducer add user

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -1,19 +1,24 @@
 import * as types from './../../constants/actionTypes'
 
-const users = (state=[] , action) => {
-    switch(action.type){
+const users = (state = [], action) => {
+    switch (action.type) {
         case types.RECIEVE_USERS:
             return action.users
+        case types.ADD_USER:
+            return [
+                ...state,
+                action.user
+            ]
         default:
             return state
     }
 }
 
-const loginedUser = (state={} , action) => {
-    if(action.type === types.RECIEVE_LOGINED_USER){
+const loginedUser = (state = {}, action) => {
+    if (action.type === types.RECIEVE_LOGINED_USER) {
         return action.user
     }
     return state
 }
 
-export {users, loginedUser}
+export { users, loginedUser }
