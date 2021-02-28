2f508ad58da6ef73713845515a5ddc57255374fe
real7alireza <real7alireza@gmail.com>
2021-02-28 13:21:28 +0330

    local api location

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { connect } from "react-redux";
 import { useEffect } from "react";
 import Layout from "./hoc/layout";
 import useFetch from "./hooks";
-
+import { users, comments } from './api'
 
 
 const App = (props) => {
@@ -17,17 +17,6 @@ const App = (props) => {
   const { data } = useFetch(
     "https://still-headland-88471.herokuapp.com/api/applications/get"
   )
-
-  const comments = [
-    { "username": "علیرضا", "comment": "عالی عالی نصب کنید", "gender": "male" }, { "username": "محمدحسین", "comment": "بازی بسیار خوبیه دانلود کنید ممنون از سازندش", "gender": "male" }, { "username": "پریسا", "comment": "سلام لطفا کنترل با فرمان هم در بازی قرار دهید", "gender": "female" }, { "username": "احمد", "comment": "خیلی بازی بچگونس", "gender": "male" },
-    { "username": "سارا", "comment": "عالی و پرتقالی🍊🍊", "gender": "female" }, { "username": "ریحانه", "comment": "بازی بسیار خوبیه دانلود کنید ممنون از سازندش", "gender": "female" }, { "username": "امیر", "comment": "خوب است بازی", "gender": "male" }, { "username": "مریم", "comment": "برنامه ی جالبیه خوشم اومد", "gender": "female" },
-    { "username": "زینب", "comment": "نرم افزار عالی هست فقط یکم کند هستش ...", "gender": "female" }, { "username": "امیرعلی", "comment": "لایک", "gender": "male" }, { "username": "شیوا", "comment": "خوبه ولی بعضی وقت ها طبلیق میاره و هنگ میکنه", "gender": "female" }, { "username": "ممد", "comment": "بدک نیس", "gender": "male" },
-  ]
-
-  const users = [
-    { "username": "Alireza", "password": "1234", "gender": "male" }, { "username": "MohammadHossein", "password": "5678", "gender": "male" }
-  ]
-
 
   useEffect(() => {
     props.dispatch(action.recieveUsers(users))
---
+++ src/api/index.js
@@ -0,0 +1,21 @@
+export const comments = [
+    { "username": "علیرضا", "comment": "عالی عالی نصب کنید", "gender": "male", "id": "4f67678bdc2b4b2d837bbf1558c14e2d" },
+    { "username": "محمدحسین", "comment": "بازی بسیار خوبیه دانلود کنید ممنون از سازندش", "gender": "male", "id": "d0e98092d5eb480ebdf4e60443f7a862" },
+    { "username": "پریسا", "comment": "سلام لطفا کنترل با فرمان هم در بازی قرار دهید", "gender": "female", "id": "25f3dbbf326a4287a113ff3cfbd2bbd7" },
+    { "username": "احمد", "comment": "خیلی بازی بچگونس", "gender": "male", "id": "d7b38713b0314e3082d172f53ea34ebc" },
+    { "username": "سارا", "comment": "عالی و پرتقالی🍊🍊", "gender": "female", "id": "b0e6324f4f9f47f3b508fe184e1c4270" },
+    { "username": "ریحانه", "comment": "بازی بسیار خوبیه دانلود کنید ممنون از سازندش", "gender": "female", "id": "a8f9cb47986e4601bf3c3900e17a933f" },
+    { "username": "امیر", "comment": "خوب است بازی", "gender": "male", "id": "0bd07efe65ed48ab929e04772f300f04" },
+    { "username": "مریم", "comment": "برنامه ی جالبیه خوشم اومد", "gender": "female", "id": "c8573fd0fc104f9dbb5f5f46cc5a40ba" },
+    { "username": "زینب", "comment": "نرم افزار عالی هست فقط یکم کند هستش ...", "gender": "female", "id": "239ca3672241443886d808d91571bccb" },
+    { "username": "امیرعلی", "comment": "لایک", "gender": "male", "id": "997262c873aa475f995f8dc447de6ee1" },
+    { "username": "شیوا", "comment": "خوبه ولی بعضی وقت ها طبلیق میاره و هنگ میکنه", "gender": "female", "id": "de8fa249ee4c439ea35bb7ad10a93f57" },
+    { "username": "ممد", "comment": "بدک نیس", "gender": "male", "id": "3cccb90d808b45b19f48804ed03a86b5" },
+]
+
+export const users = [
+    { "username": "Alireza", "password": "1234", "gender": "male", "id": "723c1c7f83b94107abaca6a9b224d824" },
+    { "username": "MohammadHossein", "password": "5678", "gender": "male", "id": "8b87954822fb475485143287bc695dd0" }
+]
+
+export const cartProducts = []
