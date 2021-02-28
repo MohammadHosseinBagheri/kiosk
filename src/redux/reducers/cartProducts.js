43d5ac69367f8562b00dcd68bb831f7f9a6425dc
real7alireza <real7alireza@gmail.com>
2021-02-28 13:49:52 +0330

    reduce add to cart

--- src/redux/reducers/cartProducts.js
+++ src/redux/reducers/cartProducts.js
@@ -1,9 +1,14 @@
-import { RECIEVE_CART_PRODUCTS } from './../../constants/actionTypes'
+import { RECIEVE_CART_PRODUCTS, ADD_TO_CART } from './../../constants/actionTypes'
 
 const cartProducts = (state = [], action) => {
     switch (action.type) {
         case RECIEVE_CART_PRODUCTS:
             return action.cartProducts
+        case ADD_TO_CART:
+            return [
+                ...state,
+                action.product
+            ]
         default:
             return state
     }
