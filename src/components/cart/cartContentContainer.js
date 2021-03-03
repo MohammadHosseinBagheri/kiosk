917c75fbc08d4da0767f0c1b7ec43c3e2bb17303
real7alireza <real7alireza@gmail.com>
2021-03-03 23:17:50 +0330

    total price box in cart

--- src/components/cart/cartContentContainer.js
+++ src/components/cart/cartContentContainer.js
@@ -3,16 +3,31 @@ import { Container, Col, Row } from 'react-bootstrap'
 import CartProduct from './cartProduct'
 
 const CartContentContainer = props => {
-    console.log(props)
+    // console.log(props)
     const { products } = props
+    console.log(products)
     return (
-        <Container>
+        <Container dir='rtl'>
             <Row>
-                {products.map(product => {
-                    <Col>
-                        <CartProduct {...product} />
-                    </Col>
-                })}
+                <Col sm={12} md={9}>
+                    {products.map(product => {
+                        return <CartProduct {...product} />
+                    })}
+                </Col>
+                <Col sm={12} md={3} >
+                    <div 
+                        className='position-fixed border border-info my-3 rounded '
+                        style={{
+                            'boxSizing': 'border-box'
+                        }}>
+                        <Col xs={12} className='my-5'>
+                            <div>مجموع سبد خرید</div>
+                        </Col>
+                        <Col xs={12} className='my-5'>
+                            <div>یک میلیون تومن</div>
+                        </Col>
+                    </div>
+                </Col>
             </Row>
         </Container>
     )
