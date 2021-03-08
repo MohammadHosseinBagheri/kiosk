508a4623e5a592e5cde2cfd7f67967264dbac9dd
real7alireza <real7alireza@gmail.com>
2021-03-08 11:22:41 +0330

    cart product responsive

--- src/components/cart/cartProduct.js
+++ src/components/cart/cartProduct.js
@@ -3,11 +3,11 @@ import img from './../../assets/icon/app.png'
 
 
 const CartProduct = props => {
-    console.log(props)
     return (
         <Row className=' bg-light my-3 rounded'>
-            <Col xs={12} sm={3} className='text-center' >
-                <img className='w-100' style={{ 'maxWidth': '200px' }} src={img} />
+            <Col xs={5} sm={3} className='text-center d-flex flex-column justify-content-around align-items-center' >
+                <img className='w-100' style={{ 'maxWidth': '150px' }} src={img} />
+                <Button variant='danger' className='d-sm-none'>حذف</Button>
             </Col>
             <Col xs={6} sm={6} className='d-flex flex-column justify-content-between align-items-start bd-highlight mb-3' >
                 <Row className='text-right mt-3' >
@@ -15,13 +15,14 @@ const CartProduct = props => {
                     <Col xs={12}>{props.companyName}</Col>
                     <Col xs={12}>{props.tag}</Col>
                 </Row>
-                <Row xs={12}>
+                <Row xs={12} >
                     <Col>
-                        <Button variant='danger'>حذف از سبد خربد</Button>
+                        <Button variant='danger d-none d-sm-block'>حذف از سبد خربد</Button>
+                        <strong className='d-sm-none' >100 تومن</strong>
                     </Col>
                 </Row>
             </Col>
-            <Col xs={6} sm={3} className='d-flex justify-content-end align-items-end mb-4 pl-5'>
+            <Col xs={0} sm={3} className='d-none d-sm-flex justify-content-end align-items-end mb-4 pl-5'>
                 <strong>100 تومن</strong>
             </Col>
         </Row>
