0ee57f9f1ba619eeab11a76f70d5451e3d7ca47c
real7alireza <real7alireza@gmail.com>
2021-03-08 15:16:44 +0330

    cart total price style

--- src/components/cart/cartContentContainer.js
+++ src/components/cart/cartContentContainer.js
@@ -1,31 +1,39 @@
 import { connect } from 'react-redux'
-import { Container, Col, Row } from 'react-bootstrap'
+import { Container, Col, Row, Button } from 'react-bootstrap'
 import CartProduct from './cartProduct'
 
 const CartContentContainer = props => {
     const { products } = props
-    console.log(products)
     return (
-        <Container dir='rtl'>
-            <Row>
-                <Col sm={12} md={9}>
-                    {products.map(product => {
-                        return <CartProduct {...product} />
-                    })}
-                </Col>
-                <Col sm={12} md={3}>
-                    <div
-                        className='cartTotalPrice '>
-                        <Col xs={12} className='my-5'>
-                            <div>مجموع سبد خرید</div>
-                        </Col>
-                        <Col xs={12} className='my-5'>
-                            <div>یک میلیون تومن</div>
-                        </Col>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            <Container dir='rtl'>
+                <Row>
+                    <Col sm={12} md={8}>
+                        {products.map(product => {
+                            return <CartProduct {...product} />
+                        })}
+                    </Col>
+                    <Col sm={12} md={4} className=' d-none d-md-block' >
+                        <Row className='cartTotalPriceContent bg-light mt-5 mr-1 text-center'>
+                            <Col xs={12} className='px-0 border-bottom py-2'>
+                                <div className='small'>جمع سبد خرید</div>
+                            </Col>
+                            <Col xs={12} className='px-0 border-bottom py-2'>
+                                <div className='small'>375000<span> تومن</span></div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+            <Container dir='rtl' className='text-center'>
+                <Row className='d-unset d-md-none fixed-bottom bg-info' >
+                    <Col xs={6}>
+                        <div className='small'>جمع سبد خرید</div>
+                        <div className='small'>375000<span> تومن</span></div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
 
