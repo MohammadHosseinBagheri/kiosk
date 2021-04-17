d4361d5abc5765a4d954575a583942d4fe5af927
real7alireza <real7alireza@gmail.com>
2021-04-18 02:47:14 +0430

    change side drawer icon

--- src/components/navigation/sideDrawer/drawerToggle/drawerToggle.js
+++ src/components/navigation/sideDrawer/drawerToggle/drawerToggle.js
@@ -4,21 +4,11 @@ import { sideDrawerToggle } from './../../../../redux/actions'
 
 const DrawerToggle = (props) => {
   return (
-    <Button className='drawerToggle' onClick={() => props.dispatch(sideDrawerToggle())}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        className="bi bi-justify"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fillRule="evenodd"
-          d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
-        />
+    <Button className='drawerToggle' variant='muted' onClick={() => props.dispatch(sideDrawerToggle())}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
       </svg>
-    </Button>
+    </Button >
   );
 };
 
