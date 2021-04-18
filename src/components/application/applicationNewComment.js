d83d25c68964496f287efd1337a9c87e31541d56
real7alireza <real7alireza@gmail.com>
2021-04-19 01:39:21 +0430

    The user must be logged in to post a comment

--- src/components/application/applicationNewComment.js
+++ src/components/application/applicationNewComment.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Form, Row, Col, Button, Modal } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { addComments } from './../../redux/actions'
+import Login from './../account/login'
 
 const ApplicationNewComment = props => {
 
@@ -10,6 +11,14 @@ const ApplicationNewComment = props => {
     const [username, setUsername] = useState('')
     const [comment, setComment] = useState('')
     const [showSuccessModal, setShowSuccessModal] = useState(false)
+    const [showLoginModal, setShowLoginModal] = useState(false)
+
+    const showLoginModalHandler = () => {
+        setShowLoginModal(true)
+    }
+    const hideLoginModalHandler = () => {
+        setShowLoginModal(false)
+    }
 
     const nameChangedHandler = (event) => {
         setUsername(event.target.value)
@@ -34,24 +43,40 @@ const ApplicationNewComment = props => {
         setShowSuccessModal(false)
     }
 
+    let isLoginedUser = Object.keys(props.loginedUser).length > 0
 
     return (
         <>
             <Form className='w-100 bg-light pb-5 px-5 pt-2 my-2 text-right shadow-sm rounded'>
-                <Row>
-                    <Col className='pb-4'>دیدگاه</Col>
-                </Row>
-                <Row>
-                    <Col xs={12}>
-                        <Form.Control onChange={nameChangedHandler} className='mb-3' placeholder="نام خود را وارد کنید..." value={loginedUser.username} />
-                        <Col xs={12}>
-                        </Col>
-                        <Form.Control onChange={commentChangedHandler} className='mb-4' as='textarea' rows={3} placeholder="نظرت در مورد برنامه..." />
-                    </Col>
-                    <Col xs={12} className='text-left'>
-                        <Button className='px-4 py-2' variant="outline-success" onClick={sendComment}>ثبت دیدگاه</Button>
-                    </Col>
-                </Row>
+                {
+                    isLoginedUser ?
+                        <>
+                            <Row>
+                                <Col className='pb-4'>دیدگاه</Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12}>
+                                    <Form.Control onChange={nameChangedHandler} className='mb-3' placeholder="نام خود را وارد کنید..." value={loginedUser.username} />
+                                    <Col xs={12}>
+                                    </Col>
+                                    <Form.Control onChange={commentChangedHandler} className='mb-4' as='textarea' rows={3} placeholder="نظرت در مورد برنامه..." />
+                                </Col>
+                                <Col xs={12} className='text-left'>
+                                    <Button className='px-4 py-2' variant="outline-success" onClick={sendComment}>ثبت دیدگاه</Button>
+                                </Col>
+                            </Row>
+                        </> :
+                        <>
+                            <Row>
+                                <Col className='pb-4'>ثبت دیدگاه</Col>
+                            </Row>
+                            <Row>
+                                <Col>برای ارسال دیدگاه لازم است، ابتدا
+                                <Button variant='link' onClick={showLoginModalHandler}>وارد سایت</Button>
+                                 شوید.</Col>
+                            </Row>
+                        </>
+                }
             </Form>
             <Modal
                 size='sm'
@@ -63,14 +88,11 @@ const ApplicationNewComment = props => {
                 <Modal.Body as={Form} className='text-center pt-4 mb-1'>
                     با موفقیت ثبت شد !
                </Modal.Body>
-                {/* <Modal.Footer as={Form} className='justify-content-center'> */}
                 <div className='d-flex pb-4 justify-content-center'>
-                    <Button variant='success'>تایید</Button>
+                    <Button variant='success' onClick={hideSuccessModalHandler}>تایید</Button>
                 </div>
-                {/* </Modal.Footer> */}
-
-
             </Modal>
+            <Login show={showLoginModal} hideHandle={hideLoginModalHandler} />
         </>
     )
 }
