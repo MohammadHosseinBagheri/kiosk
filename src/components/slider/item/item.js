ad474b907527c3788d79e0f80e5292b06e49bb74
Alireza <real7alireza@gmail.com>
2021-05-02 21:51:09 +0430

    remove comments from query url

--- src/components/slider/item/item.js
+++ src/components/slider/item/item.js
@@ -6,6 +6,9 @@ const Item = (props) => {
   const slideClickedHandler = () => {
     const queryParams = []
     for (let i in props.product) {
+      if(typeof(props.product[i])==='object'){
+        continue
+      }
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(props.product[i]))
     }
     const queryString = queryParams.join('&')
