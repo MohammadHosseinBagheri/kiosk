3e0871897c1d7520ac934c5d5ff989f26503255c
Alireza <real7alireza@gmail.com>
2021-05-04 01:32:47 +0430

    responsive search input

--- src/components/navigation/toolbar.js
+++ src/components/navigation/toolbar.js
@@ -19,21 +19,21 @@ const Toolbar = () => {
       <div className="desktopOnly">
         <NavLeft />
       </div>
+      <Col className="position-relative p-0 search d-none d-md-flex align-items-center justify-content-end">
+        <input className="search-text " type="text" placeholder="جستجو" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          fill="currentColor"
+          className="bi bi-search icon-search"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+      </Col>
       <nav className="desktopOnly">
         <Row>
-          <Col className="position-relative p-0 search d-flex align-items-center">
-            <input className="search-text" type="text" placeholder="جستجو" />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              fill="currentColor"
-              className="bi bi-search icon-search"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-            </svg>
-          </Col>
           <Col className="pr-0">
             <NavigationItems />
           </Col>
@@ -51,9 +51,30 @@ const Toolbar = () => {
           </Col>
         </Row>
       </nav>
-      <div className="d-flex d-sm-none justify-content-between w-100">
-        <img src={img} />
-        <DrawerToggle />
+      <div className="d-flex d-md-none justify-content-between w-100">
+        <Col
+          xs={9}
+          className="position-relative p-0 search d-flex align-items-center pl-4"
+        >
+          <input
+            className="search-text w-100"
+            type="text"
+            placeholder="جستجو"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            fill="currentColor"
+            className="bi bi-search icon-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </Col>
+        <Col xs={3} className="pr-0 text-right">
+          <DrawerToggle />
+        </Col>
       </div>
     </header>
   );
