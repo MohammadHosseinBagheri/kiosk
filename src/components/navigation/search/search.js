1efc8c2786e35204009aad7100502d35151e54f5
Alireza <real7alireza@gmail.com>
2021-05-05 13:46:27 +0430

    go to app page when click on suggestion search

--- src/components/navigation/search/search.js
+++ src/components/navigation/search/search.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { connect } from "react-redux";
 
 const Search = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [isSuggestion, setIsSuggestion] = useState(false);
+
+  const input = useRef();
 
   const { products } = props;
 
@@ -20,24 +22,56 @@ const Search = (props) => {
   });
 
   const filterProductNames = filterProducts.map((val, key) => (
-    <li className="filterProductName" key={key}>
+    <li
+      onClick={() => goToProductPage(val)}
+      className="filterProductName"
+      key={key}
+    >
       {val.name}
     </li>
   ));
+  const goToProductPage = (product) => {
+    const queryParams = [];
+    for (let i in product) {
+      if (typeof product[i] === "object") {
+        continue;
+      }
+      queryParams.push(
+        encodeURIComponent(i) + "=" + encodeURIComponent(product[i])
+      );
+    }
+    const queryString = queryParams.join("&");
+    props.url.history.push({
+      pathname: "/app",
+      search: "?" + queryString,
+    });
 
+    setIsSuggestion(false);
+    setSearchTerm("");
+    input.current.value = "";
+  };
+
+  const noSuggestion = () => {
+    setTimeout(() => {
+      setIsSuggestion(false);
+    }, 100);
+  };
   return (
     <>
       <input
         className={`${
-          suggestion === "d-block" && isSuggestion === true ? "search-suggestion-mode" : ""
+          suggestion === "d-block" && isSuggestion === true
+            ? "search-suggestion-mode"
+            : ""
         } ${props.className} search-text `}
         type="text"
         placeholder="جستجو"
         onChange={(event) => {
           setSearchTerm(event.target.value);
         }}
-        onBlur={() => setIsSuggestion(false)}
+        onBlur={noSuggestion}
         onFocus={() => setIsSuggestion(true)}
+        ref={input}
       />
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -50,9 +84,7 @@ const Search = (props) => {
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
       </svg>
       <ul
-        className={ `filterProductNames ${
-          isSuggestion ? suggestion : "d-none"
-        }`}
+        className={`filterProductNames ${isSuggestion ? suggestion : "d-none"}`}
       >
         {filterProductNames}
       </ul>
@@ -60,6 +92,9 @@ const Search = (props) => {
   );
 };
 
-const mapStateToProps = (state) => ({ products: state.allProducts });
+const mapStateToProps = (state) => ({
+  products: state.allProducts,
+  url: state.homeUrlParams,
+});
 
 export default connect(mapStateToProps)(Search);
