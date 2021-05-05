1f91bfe05d813e5aca2f9745f62e6484e4094784
Alireza <real7alireza@gmail.com>
2021-05-05 14:36:48 +0430

    save about url params to redux

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,7 +1,16 @@
 import AboutContainer from "./../containers/aboutContainer";
-
+import { connect } from "react-redux";
+import { aboutUrlParams } from "./../redux/actions";
 const About = () => {
+  const urlParams = {
+    history: props.history,
+    match: props.match,
+    location: props.location,
+  };
+  useEffect(() => {
+    props.dispatch(aboutUrlParams(urlParams));
+  });
   return <AboutContainer />;
 };
 
-export default About;
+export default connect()(About);
