62f7507c50edd0efe5b2c602399dd8335c4bec2f
Alireza <real7alireza@gmail.com>
2021-05-05 14:35:06 +0430

    save cart patams to redux

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,7 +1,17 @@
-import CartContainer from './../containers/cartContainer'
+import CartContainer from "./../containers/cartContainer";
+import { connect } from "react-redux";
+import { cartUrlParams } from "./../redux/actions";
 
 const Cart = () => {
-    return <CartContainer /> 
-}
+  const urlParams = {
+    history: props.history,
+    match: props.match,
+    location: props.location,
+  };
+  useEffect(() => {
+    props.dispatch(cartUrlParams(urlParams));
+  }, []);
+  return <CartContainer />;
+};
 
-export default Cart
+export default connect()(Cart);
