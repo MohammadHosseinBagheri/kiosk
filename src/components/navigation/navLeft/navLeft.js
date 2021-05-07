6db8e07166b7bf2135f7fcf1a1cefa1077cf33f8
Alireza <real7alireza@gmail.com>
2021-05-07 19:36:47 +0430

    close side drawer handler

--- src/components/navigation/navLeft/navLeft.js
+++ src/components/navigation/navLeft/navLeft.js
@@ -1,38 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { logIn } from "./../../../constants";
-import { NavLink } from 'react-router-dom'
-import Login from './../../account/login'
-import { connect } from 'react-redux'
-import User from './../../account/userModal'
+import { NavLink } from "react-router-dom";
+import Login from "./../../account/login";
+import { connect } from "react-redux";
+import User from "./../../account/userModal";
+import { sideDrawerClosed } from "./../../../redux/actions";
 
 const NavLeft = (props) => {
-
-  const [showLoginModal, setShowLoginModal] = useState(false)
-  const [showUserModal, setShowUserModal] = useState(false)
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showUserModal, setShowUserModal] = useState(false);
 
   const showLoginModalHandler = () => {
-    setShowLoginModal(true)
-  }
+    props.dispatch(sideDrawerClosed());
+    setShowLoginModal(true);
+  };
   const hideLoginModalHandler = () => {
-    setShowLoginModal(false)
-  }
+    setShowLoginModal(false);
+  };
 
   const showUserModalHandler = () => {
-    setShowUserModal(true)
-  }
+    props.dispatch(sideDrawerClosed());
+    setShowUserModal(true);
+  };
   const hideUserModalHandler = () => {
-    setShowUserModal(false)
-  }
+    setShowUserModal(false);
+  };
 
-
-
-  let isLoginedUser = Object.keys(props.loginedUser).length > 0
+  let isLoginedUser = Object.keys(props.loginedUser).length > 0;
 
   return (
     <div className={[props.className, "navLeft"].join(" ")}>
-      <NavLink to='/cart'>
-        <Button variant="success">
+      <NavLink to="/cart">
+        <Button
+          variant="success"
+          onClick={() => props.dispatch(sideDrawerClosed())}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -40,28 +43,42 @@ const NavLeft = (props) => {
             fill="currentColor"
             className="bi bi-cart4"
             viewBox="0 0 16 16"
-          ><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+          >
+            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
           </svg>
         </Button>
       </NavLink>
-      {
-        isLoginedUser
-          ? <Button onClick={showUserModalHandler} variant="light" className="ml-2">{props.loginedUser.username}</Button>
-          : <Button onClick={showLoginModalHandler} variant="light" className="ml-2">
-            {logIn}
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-            </svg>
-          </Button>
-      }
+      {isLoginedUser ? (
+        <Button onClick={showUserModalHandler} variant="light" className="ml-2">
+          {props.loginedUser.username}
+        </Button>
+      ) : (
+        <Button
+          onClick={showLoginModalHandler}
+          variant="light"
+          className="ml-2"
+        >
+          {logIn}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            className="bi bi-person"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+          </svg>
+        </Button>
+      )}
       <Login show={showLoginModal} hideHandle={hideLoginModalHandler} />
       <User show={showUserModal} hideHandle={hideUserModalHandler} />
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  loginedUser: state.loginedUser
-})
+const mapStateToProps = (state) => ({
+  loginedUser: state.loginedUser,
+});
 
 export default connect(mapStateToProps)(NavLeft);
