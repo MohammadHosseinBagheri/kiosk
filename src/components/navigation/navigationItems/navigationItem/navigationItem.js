6a7e1c43b61484cafe93c47b18f30fdb166c6609
Alireza <real7alireza@gmail.com>
2021-05-07 19:36:31 +0430

    close side drawer handler

--- src/components/navigation/navigationItems/navigationItem/navigationItem.js
+++ src/components/navigation/navigationItems/navigationItem/navigationItem.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { sideDrawerClosed } from "./../../../../redux/actions";
 
 const navigationItem = (props) => {
   const { link, exact, children } = props;
   return (
-    <li className="navigationItem">
+    <li
+      className="navigationItem"
+      onClick={() => props.dispatch(sideDrawerClosed())}
+    >
       <NavLink to={link} exact={exact}>
         {children}
       </NavLink>
@@ -12,4 +17,8 @@ const navigationItem = (props) => {
   );
 };
 
-export default navigationItem;
+// const mapStateToProps = state => {
+//   sideDrawerClosed
+// }
+
+export default connect()(navigationItem);
