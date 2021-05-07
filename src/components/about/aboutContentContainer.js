e2204d7a6f9b8f501f2cec99c1a185de2e79f61c
Alireza <real7alireza@gmail.com>
2021-05-07 18:02:52 +0430

    add some content on about page

--- src/components/about/aboutContentContainer.js
+++ src/components/about/aboutContentContainer.js
@@ -1,8 +1,127 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const AboutContentContainer = () => {
-  return <Container className="about text-right p-5"></Container>;
+  return (
+    <Container className="about text-center position-relative">
+      <div
+        dir="rtl"
+        className="position-absolute about-us flex-column text-dark pr-3"
+      >
+        <Row className="m-0">
+          <h3>درباره ما</h3>
+        </Row>
+        <Row className="m-0">
+          <p>با ما به تمام برنامه های دنیا دسترسی داشته باشید :)</p>
+        </Row>
+      </div>
+      <Row className="first-about-icon">
+        <Col xs={2}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="72"
+            height="72"
+            fill="currentColor"
+            class="bi bi-dice-1"
+            viewBox="0 0 16 16"
+          >
+            <circle cx="8" cy="8" r="1.5" />
+            <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z" />
+          </svg>
+        </Col>
+        <Col xs={10}></Col>
+      </Row>
+
+      <Row>
+        <Col xs={2}></Col>
+        <Col xs={2}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="72"
+            height="72"
+            fill="currentColor"
+            class="bi bi-dice-2"
+            viewBox="0 0 16 16"
+          >
+            <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z" />
+            <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+          </svg>
+        </Col>
+        <Col xs={8}></Col>
+      </Row>
+
+      <Row>
+        <Col xs={4}></Col>
+        <Col xs={2}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="72"
+            height="72"
+            fill="currentColor"
+            class="bi bi-dice-3"
+            viewBox="0 0 16 16"
+          >
+            <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z" />
+            <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-4-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+          </svg>
+        </Col>
+        <Col xs={6}></Col>
+      </Row>
+
+      <Row>
+        <Col xs={6}></Col>
+        <Col xs={2}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="72"
+            height="72"
+            fill="currentColor"
+            class="bi bi-dice-4"
+            viewBox="0 0 16 16"
+          >
+            <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z" />
+            <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+          </svg>
+        </Col>
+        <Col xs={4}></Col>
+      </Row>
+
+      <Row>
+        <Col xs={8}></Col>
+        <Col xs={2}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="72"
+            height="72"
+            fill="currentColor"
+            class="bi bi-dice-5"
+            viewBox="0 0 16 16"
+          >
+            <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z" />
+            <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm4-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+          </svg>
+        </Col>
+        <Col xs={2}></Col>
+      </Row>
+
+      <Row className="d-none d-sm-flex">
+        <Col xs={10}></Col>
+        <Col xs={2}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="72"
+            height="72"
+            fill="currentColor"
+            class="bi bi-dice-6"
+            viewBox="0 0 16 16"
+          >
+            <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z" />
+            <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-8 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+          </svg>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default AboutContentContainer;
