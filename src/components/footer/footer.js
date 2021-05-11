da3161c7d767b822a9ad8102291bf0fc8aad0242
Alireza <real7alireza@gmail.com>
2021-05-12 00:48:13 +0430

    footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  return <Container dir="rtl"></Container>;
+};
+
+export default Footer;
--- src/components/home/homeContentContainer.js
+++ src/components/home/homeContentContainer.js
@@ -1,26 +1,24 @@
 import Slider from "./../slider/slider";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
+import Footer from "./../footer/footer";
 
 const HomeContentContainer = (props) => {
-
-  const { bestProducts, newestProducts } = props
-
+  const { bestProducts, newestProducts } = props;
 
   return (
-    <div className='home'>
-      <Slider products={bestProducts} />
-      <Slider products={newestProducts} />
-    </div>
+    <>
+      <div className="home">
+        <Slider products={bestProducts} />
+        <Slider products={newestProducts} />
+      </div>
+      <Footer />
+    </>
   );
 };
 
-
-const mapStateToProps = (state) => (
-  {
-    bestProducts: state.bestProducts,
-    newestProducts: state.newestProducts
-  }
-)
-
+const mapStateToProps = (state) => ({
+  bestProducts: state.bestProducts,
+  newestProducts: state.newestProducts,
+});
 
 export default connect(mapStateToProps)(HomeContentContainer);
