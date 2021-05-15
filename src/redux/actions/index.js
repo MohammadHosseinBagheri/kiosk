30016f1c4035f9e42383d0f12d77bd0db3745d88
Alireza <real7alireza@gmail.com>
2021-05-16 03:04:02 +0430

    action receive free products

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -6,79 +6,86 @@ export const receiveProducts = (products) => ({
 });
 
 export const sideDrawerClosed = () => ({
-  type: types.SIDE_DRAWER_CLOSED
-})
+  type: types.SIDE_DRAWER_CLOSED,
+});
 
 export const sideDrawerToggle = () => ({
-  type: types.SIDE_DRAWER_TOGGLE
-})
+  type: types.SIDE_DRAWER_TOGGLE,
+});
 
-export const receiveBestProducts = products => ({
+export const receiveBestProducts = (products) => ({
   type: types.RECEIVE_BEST_PRODUCTS,
-  products
-})
+  products,
+});
 
-export const receiveNewestProducts = products => ({
+export const receiveNewestProducts = (products) => ({
   type: types.RECEIVE_NEWEST_PRODUCTS,
-  products
-})
+  products,
+});
 
-export const homeUrlParams = homeUrlParams => ({
+export const receiveFreeProducts = (products) => ({
+  type: types.RECEIVE_FREE_PRODUCTS,
+  products,
+});
+
+export const homeUrlParams = (homeUrlParams) => ({
   type: types.HOME_URL_PARAMS,
-  homeUrlParams
-})
+  homeUrlParams,
+});
 
-export const appUrlParams = appUrlParams => ({
+export const appUrlParams = (appUrlParams) => ({
   type: types.APP_URL_PARAMS,
-  appUrlParams
-})
+  appUrlParams,
+});
 
-export const cartUrlParams = cartUrlParams => ({
+export const cartUrlParams = (cartUrlParams) => ({
   type: types.CART_URL_PARAMS,
-  cartUrlParams
-})
+  cartUrlParams,
+});
 
-export const aboutUrlParams = aboutUrlParams => ({
+export const aboutUrlParams = (aboutUrlParams) => ({
   type: types.ABOUT_URL_PARAMS,
-  aboutUrlParams
-})
+  aboutUrlParams,
+});
 
-export const receiveComments = comments => ({
+export const receiveComments = (comments) => ({
   comments,
   type: types.RECIEVE_COMMENTS,
 });
 
-export const addComments = comment => ({
+export const addComments = (comment) => ({
   comment,
   type: types.ADD_COMMENT,
 });
 
-export const recieveUsers = users => ({
+export const recieveUsers = (users) => ({
   users,
-  type: types.RECIEVE_USERS
-})
+  type: types.RECIEVE_USERS,
+});
 
-export const recieveLoginedUser = user => ({
+export const recieveLoginedUser = (name, email, password) => ({
+  name,
+  email,
+  password,
+  type: types.RECIEVE_LOGINED_USER,
+});
+
+export const addUser = (user) => ({
   user,
-  type: types.RECIEVE_LOGINED_USER
-})
+  type: types.ADD_USER,
+});
 
-export const addUser = user => ({
-  user,
-  type: types.ADD_USER
-})
-
-export const recieveCartProducts = cartProducts => ({
+export const recieveCartProducts = (cartProducts) => ({
   type: types.RECIEVE_CART_PRODUCTS,
-  cartProducts
-})
+  cartProducts,
+});
 
-export const addToCart = product => ({
+export const addToCart = (product) => ({
   type: types.ADD_TO_CART,
-  product
-})
+  product,
+});
 
-export const removeFromCart = id => ({
+export const removeFromCart = (id) => ({
   type: types.REMOVE_FROM_CART,
-  id
-})
+  id,
+});
