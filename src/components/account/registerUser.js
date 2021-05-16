4f70df7dde1c9c5c7829416f82fb76486c75385a
Alireza <real7alireza@gmail.com>
2021-05-16 18:33:38 +0430

    post request signup

--- src/components/account/registerUser.js
+++ src/components/account/registerUser.js
@@ -1,70 +1,89 @@
-import React, { useState } from 'react'
-import { Modal, Button, Form, Col } from 'react-bootstrap'
-import {connect} from 'react-redux'
-import {addUser, recieveLoginedUser} from './../../redux/actions'
+import React, { useState } from "react";
+import { Modal, Button, Form, Col } from "react-bootstrap";
+import { connect } from "react-redux";
+import { addUser, recieveLoginedUser } from "./../../redux/actions";
+import axios from "axios";
 
-const RegisterUser = props => {
-    
-    
-    
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [gender, setGender] = useState('')
+const RegisterUser = (props) => {
+  const [name, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmali] = useState("");
 
-    const usernameChangedHandler = event => {
-        setUsername(event.target.value)
+  const usernameChangedHandler = (event) => {
+    setUsername(event.target.value);
+  };
 
-    }
+  const passwordChangedHandler = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const passwordChangedHandler = event => {
-        setPassword(event.target.value)
-    }
+  const emailChangedHandler = (event) => {
+    setEmali(event.target.value);
+  };
 
-    const genderChangedHandler = event => {
-        setGender(event.target.id)
-    }
+  const sendNewUser = () => {
+    // post data
+    const user = { email, password, name };
+    axios
+      .post("https://still-headland-88471.herokuapp.com/api/register", user)
+      .then((res) => {
+        console.log("success ", res);
+        props.dispatch(recieveLoginedUser(user));
+        props.hideRegisterModal();
+      })
+      .catch((err) => console.log(err));
 
-    const sendNewUser = () => {
-        // post data
-        const user = {
-            username,
-            password,
-            gender
-        }
-        props.dispatch(addUser(user))
-        props.dispatch(recieveLoginedUser(user))
-        props.hideRegisterModal()
-    }
+    // props.dispatch(recieveLoginedUser(user));
+    // props.dispatch(addUser(user));
+    // props.hideRegisterModal();
+  };
 
-    
-    
-    return (
-        <Modal
-            show={!props.registered}
-            onHide={props.hideRegisterModal}
-            dir='rtl'
-            centered
-        >
-            <Modal.Header >
-                <Modal.Title >ثبت نام</Modal.Title>
-            </Modal.Header>
+  return (
+    <Modal
+      show={!props.registered}
+      onHide={props.hideRegisterModal}
+      dir="rtl"
+      centered
+    >
+      <Modal.Header>
+        <Modal.Title>ثبت نام</Modal.Title>
+      </Modal.Header>
 
-            <Modal.Body as={Form} className='text-right'>
-                <Form.Control onChange={usernameChangedHandler} type="text" placeholder="نام کاربری" className='mb-3' />
-                <Form.Control onChange={passwordChangedHandler} type="password" placeholder="رمز" />
-                <Col className='d-flex genderCheck'>
-                    <Form.Check onChange={genderChangedHandler} name='gender' id='male' type='radio' label='آقا' />
-                    <Form.Check onChange={genderChangedHandler} name='gender' id='female' type='radio' label='خانم' />
-                </Col>
-            </Modal.Body>
+      <Modal.Body as={Form} className="text-right">
+        <Form.Control
+          onChange={usernameChangedHandler}
+          type="text"
+          placeholder="نام خود را وارد کنید"
+          className="mb-3"
+        />
+        <Form.Control
+          required
+          onChange={emailChangedHandler}
+          type="email"
+          placeholder="ایمیل خود را وارد کنید"
+          className="mb-3"
+        />
+        <Form.Control
+          onChange={passwordChangedHandler}
+          type="password"
+          placeholder="رمز خود را وارد کنید"
+        />
+      </Modal.Body>
 
-            <Modal.Footer>
-                <Col xs={12}>
-                    <Button onClick={sendNewUser} variant="primary" className='w-100' style={{ borderRadius: '100px' }}>ثبت نام</Button>
-                </Col>
-            </Modal.Footer>
-        </Modal>
-    )
-}
+      <Modal.Footer>
+        <Col xs={12}>
+          <Button
+            onClick={sendNewUser}
+            variant="primary"
+            className="w-100"
+            style={{ borderRadius: "100px" }}
+          >
+            ثبت نام
+          </Button>
+        </Col>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-export default connect()(RegisterUser)
+export default connect()(RegisterUser);
